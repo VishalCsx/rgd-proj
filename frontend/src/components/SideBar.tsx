4b1d4bdd7ee8ea39a40b6bc70ce573cf57395238
VishalCsx <vishalcsx@gmail.com>
2025-07-17 16:52:06 +0530

    chore:create folder sturcture for the sidebar component

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,8 +1,37 @@
+import { Link } from "react-router-dom";
 const SideBar = () => {
   return (
     <div>
       <nav>
-        <div></div>
+        <div className="m-1 bg-black">
+          <div>RGD modern Acadamy</div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">DashBoard</Link>
+              </li>
+              <li>
+                <Link to="/students">Student</Link>
+              </li>
+              <li>
+                <Link to="/fee-management">Fee Management</Link>
+              </li>
+              <li>
+                <Link to="/staff-management">Staff</Link>
+              </li>
+
+              <li>
+                <Link to="/expense-management">Expenses</Link>
+              </li>
+              <li>
+                <Link to="/result">Result</Link>
+              </li>
+              <li>
+                <Link to="/sign-out">Sign out</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   );
