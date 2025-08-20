3cd4ebb567a797ab1d29418fbcb3ff164cb05d2c
VishalCsx <vishalcsx@gmail.com>
2025-08-20 11:00:46 +0530

    setup the all app

--- packages/db/prisma/migrations/20250817120923_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Student" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "fatherName" TEXT,
-    "motherName" TEXT,
-
-    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
-);
